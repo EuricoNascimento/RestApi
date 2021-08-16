6a19288f57b04fc7d4c0e2e27422c541762fc285
Eurico Neto <euriconascimentofn@gmail.com>
2021-08-16 15:56:34 -0300

    Recriado o projeto

---
+++ src/main/kotlin/com/praxia/springcontactmysql/SpringContactMysqlApplication.kt
@@ -0,0 +1,11 @@
+package com.praxia.springcontactmysql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringContactMysqlApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringContactMysqlApplication>(*args)
+}
---
+++ src/test/kotlin/com/praxia/springcontactmysql/SpringContactMysqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.praxia.springcontactmysql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringContactMysqlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
